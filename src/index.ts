9373cb84b2a3f0e1fa0577d831b1ea2e72842a71
Elvin Ali <ali.elvin@gmail.com>
2024-06-17 13:29:14 +0300

    eat: Dockerize and add GraphiQL - Dockerized - Added GraphiQL - Added .env template

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,23 @@ import express from 'express';
 import { createHandler } from 'graphql-http/lib/use/express';
 import schema from './schema';
 import resolvers from './resolvers';
+// @ts-ignore
+import { ruruHTML } from "ruru/server";
 
 const app = express();
 
 app.use('/graphql', createHandler({ schema, rootValue: resolvers }));
 
-app.listen(4000, () => {
-  console.log('GraphQL server is running on http://localhost:4000/graphql');
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`GraphQL server is running on http://localhost:${PORT}/graphql`);
+});
+
+app.get("/", (req, res, next) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  return res.end(
+    ruruHTML({
+      endpoint: "/graphql",
+    }),
+  );
 });
